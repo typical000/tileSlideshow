ee687355d705a99125a422f591a1df9578fb154d
Pavel Davydov <typical000@gmail.com>
2014-10-08 23:40:02 +0300

    tileSlideshow

--- README.md
+++ README.md
@@ -1,4 +1,114 @@
-tileSlideshow
-=============
-
+# tileSlideshow
 Simple jQuery-based random slideshow plugin
+
+For more information see [DEMO](http://example.net/)
+
+Setup
+-----
+Add this tags to the `<head>` of your document (or before the `</body>` tag). This will link jQuery, Tile Slideshow CSS\JS into your webpage.
+
+```html
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="jquery.tileSlideshow.js"></script>
+<link rel="stylesheet" type="text/css" href="tileSlideshow.css" >
+```
+
+Markup
+------
+Start with a single containing element, in this example is `<div class="tile-container">`. After, create a list of items (blocks) with the same class (In this example there are `<div class="tile-item">`).
+Inside each item insert inner element that will be switched (slided) in given interval (in our example there are just `<img>` tags. Buy you can use any html tag you need)
+
+```html
+<div class="tile-container">
+  <div class="tile-item">
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+  </div>
+  <div class="tile-item">
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+  </div>
+  <div class="tile-item">
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+    <img src="#" alt=""/>
+  </div>
+</div>
+```
+
+Start the slideshow
+-------------------
+In last step, add the following lines of JavaScript into the `<head>` of your document (or before the `</body>` tag).
+The `$(window).load()` function is required to ensure the content of the page is loaded before the plugin initializes
+
+```html
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.tile-container').tileSlideshow();
+  });
+</script>
+```
+
+Change CSS to your needs
+------------------------
+
+After all, take `tileSlideshow.css` and modify it to your needs, because this plugn was build as more independent from CSS as it can be. You can change anything according to your future website design,
+except `overflow`, `opacity` and `z-index` parameters.
+
+Advanced options & access to slideshow
+--------------------------------------
+Listed below are all of the options available to customize tileSlideshow to suite your needs.
+
+```html
+animation: 'fade'        // Animation type. Supported values: 'fade', 'moveUp', 'moveDown', 'moveLeft', 'moveRight'
+animationSpeed: 500,     // Speed of switch for slided elements
+selector: '.tile-item',  // Selector for items that contain sliding images/items
+innerSelector: 'img',    // Selector for inner items that will be slided in given interval
+itemsPerInterval: 1,     // Number of random container that will be slided in given interval
+interval: 1000,          // Interval of time
+```
+
+Also, slideshow is accessible from any point of code vie jQuery `.data()` method. For example:
+```html
+$('#container-helpers').data('tileSlideshow').stop();
+```
+
+All this functions are listed below:
+
+1. switchItems
+2. play
+3. stop
+4. destroy
+
+Helper strings have been added too for performing quick actions on slideshow. All this helper strings are listed below:
+```html
+$('.tile-container').tileSlideshow('switchItems');
+$('.tile-container').tileSlideshow('play');
+$('.tile-container').tileSlideshow('stop');
+$('.tile-container').tileSlideshow('destroy');
+```
+
+Browser support
+---------------
+All modern versions of Firefox, Chrome, Safari, iOS, Android, and Internet Explorer have been tested and are supported.
+It works fine in IE8 and IE7 too. But if you support this old browsers your soul will burn in hell :)
+
+License
+-------
+
+The MIT License (MIT)
+
+Copyright © 2014 [Pavel Davydov](<typical000@gmail.com>)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
---
+++ tileSlideshow.js
@@ -0,0 +1,236 @@
+/*
+ * jQuery tileSlideshow v1.0
+ * Copyright 2014 Pavel Davydov
+ */
+
+;
+(function($){
+
+    'use strict';
+
+    $.tileSlideshow = function(item, options){
+        var $item = $(item);
+
+        // making variables public
+        $item.settings = $.extend({}, $.tileSlideshow.defaults, options);
+
+        $.data(item, 'tileSlideshow', $item);
+
+        var $target = $item.find($item.settings.selector),
+            $targetInner = $item.find($item.settings.innerSelector),
+            targetNumber = $target.length,
+            showInterval,
+            itemsIndexes = [],
+            itemsPerInterval = ($item.settings.itemsPerInterval > targetNumber) ? targetNumber : $item.settings.itemsPerInterval;
+
+        // Function for getting randow item in all collection of blocks
+        var getRandomItem = function(){
+            var currentIndex = null,
+                duplicate = false;
+
+            while(!duplicate) {
+                currentIndex = parseInt(Math.random()* targetNumber);
+                if(!isInArray(currentIndex, itemsIndexes)) {
+                    itemsIndexes.push(currentIndex);
+                    duplicate = true;
+                }
+            }
+            return $target.eq(currentIndex);
+        };
+
+        // Set count of items in inner container
+        var setInnerCount = function($target) {
+            $target.data('count', $target.find($item.settings.innerSelector).length);
+        };
+
+        // Get count of inner items inside inner container
+        var getInnerCount = function($target) {
+            return $target.data('count');
+        };
+
+        // Test if element is in array, returns true\false
+        var isInArray = function(value, array) {
+            return $.inArray(value, array) > -1;
+        };
+
+        var placeItemUnder = function($item, count, direction) {
+            $item.css('z-index', -(count - $item.index())).show().addClass('slided');
+            if(direction) {
+                $item.css(direction, '');
+            }
+        };
+
+        // Animation types
+        var animateFade = function($element, count) {
+            $element.fadeOut($item.settings.animationSpeed, function(){
+                placeItemUnder($element, count);
+            });
+        };
+
+        var animateMoveUp = function($element, count) {
+            $element.animate({
+                top : '-100%'
+            }, $item.settings.animationSpeed, function(){
+                placeItemUnder($element, count, 'top');
+            });
+        };
+
+        var animateMoveDown = function($element, count) {
+            $element.animate({
+                top : '100%'
+            }, $item.settings.animationSpeed, function(){
+                placeItemUnder($element, count, 'top');
+            });
+        };
+
+        var animateMoveLeft = function($element, count) {
+            $element.animate({
+                left : '-100%'
+            }, $item.settings.animationSpeed, function(){
+                placeItemUnder($element, count, 'left');
+            });
+        };
+
+        var animateMoveRight = function($element, count) {
+            $element.animate({
+                left : '100%'
+            }, $item.settings.animationSpeed, function(){
+                placeItemUnder($element, count, 'left');
+            });
+        };
+
+        var animateRandomMove = function($element, count) {
+            // Take random number from 0 to 3 (because we have 4 animations with element move)
+            var randomAnimation = parseInt(Math.random()* 4);
+            switch (randomAnimation) {
+                case 0:
+                    animateMoveUp($element, count);
+                    break;
+                case 1:
+                    animateMoveDown($element, count);
+                    break;
+                case 2:
+                    animateMoveLeft($element, count);
+                    break;
+                case 3:
+                    animateMoveRight($element, count);
+                    break;
+            }
+        };
+
+        // Initialization function
+        $item.init = function(){
+            // Count number of images in each item and set it to block
+            $target.each(function(element, index){
+                setInnerCount($(this));
+            });
+            $item.play();
+        };
+
+        // Destroy function
+        $item.destroy = function(){
+            // Remove all attributes, inline styles and slideshow data attribute
+            this.find($item.settings.innerSelector).removeClass('slided').removeAttr('style');
+            $.removeData(item);
+            // Clear interval forever
+            clearInterval(showInterval)
+        };
+
+        // Init main interval
+        $item.play = function(){
+            showInterval = setInterval($item.switchItems, $item.settings.interval);
+        };
+
+        // Pause sliding interval
+        $item.stop = function(){
+            clearInterval(showInterval);
+        };
+
+        // Function for switching inner items in one iteration
+        $item.switchItems = function(){
+            // Get random items (number of items we take from 'itemsPerInterval'
+            var $currentItems = getRandomItem();
+            for(var i = 0; i < itemsPerInterval - 1; i++) {
+                $currentItems = $currentItems.add(getRandomItem());
+            }
+
+            // Switch images in each block
+            $currentItems.each(function(){
+                var $this = $(this),
+                    itemCount = getInnerCount($this),
+                    $slidingItem = $this.find($item.settings.innerSelector).not('.slided').last();
+
+                // If there is no element that we can slide
+                if(!$slidingItem.length) {
+                    // If there is no elements - revert all changes to inner blocks to initial state
+                    $this.find($item.settings.innerSelector).removeClass('slided').removeAttr('style');
+                    // Select again our item
+                    $slidingItem = $this.find($item.settings.innerSelector).not('.slided').last();
+                }
+                // Animate current item and place it under other items and make visible
+                switch ($item.settings.animation) {
+                    case 'fade':
+                        animateFade($slidingItem, itemCount);
+                        break;
+                    case 'moveUp':
+                        animateMoveUp($slidingItem, itemCount);
+                        break;
+                    case 'moveDown':
+                        animateMoveDown($slidingItem, itemCount);
+                        break;
+                    case 'moveLeft':
+                        animateMoveLeft($slidingItem, itemCount);
+                        break;
+                    case 'moveRight':
+                        animateMoveRight($slidingItem, itemCount);
+                        break;
+                    case 'randomMove':
+                        animateRandomMove($slidingItem, itemCount);
+                        break;
+                    default:
+                        break;
+                }
+            });
+
+            // Clear indexes
+            itemsIndexes = [];
+        };
+
+        // Init our slideshow
+        $item.init();
+    };
+
+    $.tileSlideshow.defaults = {
+        animation: 'fade',
+        animationSpeed: 500, // Speed of switch for slided elements
+        selector: '.tile-item', // Selector for items that contain sliding images/items
+        innerSelector: 'img', // Selector for inner items that will be slided in given interval
+        itemsPerInterval: 1, // Number of random container that will be slided in given interval
+        interval: 1000 // Interval of time
+    };
+
+    $.fn.tileSlideshow = function(options){
+
+        if(options === undefined) {
+            options = {};
+        }
+
+        if(typeof options === 'object') {
+            return this.each(function(){
+                new $.tileSlideshow(this, options);
+                // Show slideshow when initialized
+                $(this).animate({opacity: 1}, 500);
+            });
+        } else {
+            // Helper strings for making quick changes to our slider
+            var $tileSlideshow = $(this).data('tileSlideshow');
+            switch (options) {
+                case 'play': $tileSlideshow.play(); break;
+                case 'stop': $tileSlideshow.stop(); break;
+                case 'destroy': $tileSlideshow.destroy(); break;
+                case 'switchItems': $tileSlideshow.switchItems(); break;
+            }
+        }
+    };
+
+})(jQuery);
